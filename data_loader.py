23986b3010f16b3bca6f0ded079b2d8266d3d4f1
BuildTools <unconfigured@null.spigotmc.org>
2022-10-24 16:28:22 -0400

    Add user sequence counts

--- data_loader.py
+++ data_loader.py
@@ -1,42 +1,51 @@
 import torch
-import os
 from tqdm import tqdm
-from sklearn.model_selection import train_test_split
 import random
 
+
 class recSysDataset(torch.utils.data.Dataset):
-    def __init__(self, root="D:\\LSTM-Based-Art-Recommendation\\data\\kcore_5_collated.txt", max_len=35, users = -1):
+    """
+    when used correctly, dataloader will return a list containing 2 tensors
+    first being the sequences of shape (batch_size, max_len)
+    second being the user's number of sequences of shape (batch_size)
+    """
+    def __init__(self, root="D:\\LSTM-Based-Art-Recommendation\\data\\kcore_5_collated.txt", max_len=35, users=-1):
         self.sequences = []
+        self.counts = []
         with open(root, 'r') as f:
             self.idx_to_item = f.readline().strip().split(",")
-            self.item_to_idx = {v:i for i, v in enumerate(self.idx_to_item)}
-            
+            self.item_to_idx = {v: i for i, v in enumerate(self.idx_to_item)}
+
             for idx, l in tqdm(enumerate(f)):
                 if users != -1 and idx >= users:
                     break
-                
+
                 split_seq = l.strip().split(",")
                 if len(split_seq) < max_len-1:
                     self.sequences += [[1] + [int(i) for i in split_seq] + [0]*(max_len-len(split_seq)-1)]
+                    self.counts += [1]
                 else:
+                    holder = []
                     while len(split_seq) > max_len-1:
-                        delimiter = random.randint(2, max_len-1) #place where random delimiter is selected
-                        self.sequences += [[1] + [int(i) for i in split_seq[:delimiter]] + [0]*(max_len-delimiter-1)]
+                        delimiter = random.randint(2, max_len-1)  # place where random delimiter is selected
+                        holder += [[1] + [int(i) for i in split_seq[:delimiter]] + [0]*(max_len-delimiter-1)]
                         split_seq = split_seq[delimiter:]
                     if split_seq:
-                        self.sequences += [[1] + [int(i) for i in split_seq] + [0]*(max_len-len(split_seq)-1)]
-        
-        random.shuffle(self.sequences)
-        self.sequences = torch.tensor(self.sequences, dtype = torch.int64).to("cuda")
-    
+                        holder += [[1] + [int(i) for i in split_seq] + [0]*(max_len-len(split_seq)-1)]
+                    self.sequences += holder
+                    self.counts += [len(holder) for _ in holder]
+        self.sequences = torch.tensor(self.sequences, dtype=torch.int64).to("cuda")
+        self.counts = torch.tensor(self.counts, dtype=torch.int64).to("cuda")
+
     def __len__(self):
         return len(self.sequences)
-    
+
     def __getitem__(self, idx):
-        return self.sequences[idx] #torch.tensor(self.sequences[idx], dtype = torch.int64).to("cuda")
-    
+        return self.sequences[idx], self.counts[idx]
+
+
 class train_val_test_split(torch.utils.data.Dataset):
-    def __init__(self, dataset, split = (.85, .1, .05), mode="train"):
+    def __init__(self, dataset, split=(.85, .1, .05), mode="train"):
         self.dataset = dataset
         self.split = split
         self.mode = 0 if mode == "train" else 1 if mode == "val" else 2 if mode == "test" else -1
@@ -49,14 +58,15 @@ class train_val_test_split(torch.utils.data.Dataset):
         # if idx+self.interval[0] >= self.interval[1]:
         #     raise StopIteration
         return self.dataset[idx + self.interval[0]]
-        
-def parse(data = "D:\\kcore_5.json"):
+
+
+def parse(data="D:\\kcore_5.json"):
     root = data.split(".")[0] + "_collated.txt"
     user_to_items = {}
-    item_to_idx = {"<PAD>":0, "<START>":1}
+    item_to_idx = {"<PAD>": 0, "<START>": 1}
     with open(data, 'r') as f:
-        for l in tqdm(f, desc = "collating"):
-            line = eval(l)
+        for raw_line in tqdm(f, desc="collating"):
+            line = eval(raw_line)
             if line["asin"] not in item_to_idx:
                 item_to_idx[line["asin"]] = len(item_to_idx)
             if line["reviewerID"] in user_to_items:
@@ -68,25 +78,26 @@ def parse(data = "D:\\kcore_5.json"):
     with open(root, 'w') as f:
         f.write(",".join(idx_to_item) + "\n")
 
-        for idx, items in tqdm(enumerate(user_to_items.values()), desc = "writing"):
+        for idx, items in tqdm(enumerate(user_to_items.values()), desc="writing"):
             if idx+1 < len(user_to_items.values()):
                 f.write(",".join(map((lambda x: str(x[0])), sorted(items, key=(lambda z: z[1])))) + "\n")
             else:
                 f.write(",".join(map((lambda x: str(x[0])), sorted(items, key=(lambda z: z[1])))))
 
+
 def get_time(line):
     try:
         return int(line["unixReviewTime"])
-    except: #happens very rarely, about 1 in 5 million when month is missing from reviewTime so unixReviewTime is not calculated
-        return (int(line["reviewTime"][-4:]) - 1970) * 32140800 #approximate the unix timestamp based on year                
+    except:  # happens very rarely, about 1 in 5 million
+        return (int(line["reviewTime"][-4:]) - 1970) * 32140800  # approximate the unix timestamp based on year
 
-    
-#==========SAMPLE USAGE==========
-# dataset = recSysDataset(max_len=20, root = "data\\user_dedup_collated.txt")
-# train_data = train_val_test_split(dataset, split=(.85,.1,.05), mode = "train")
-# val_data = train_val_test_split(dataset, split=(.85,.1,.05), mode = "val")
-# test_data = train_val_test_split(dataset, split=(.85,.1,.05), mode = "test")
 
-# dataloader = torch.utils.data.DataLoader(train_data, batch_size = 16, shuffle = True)
-# for _ in tqdm(dataloader):
+# ==========SAMPLE USAGE==========
+# dataset = recSysDataset(max_len=20, root="data\\kcore_5_collated.txt")
+# train_data = train_val_test_split(dataset, split=(.85, .1, .05), mode="train")
+# val_data = train_val_test_split(dataset, split=(.85, .1, .05), mode="val")
+# test_data = train_val_test_split(dataset, split=(.85, .1, .05), mode="test")
+#
+# dataloader = torch.utils.data.DataLoader(train_data, batch_size=16, shuffle=True)
+# for batch in tqdm(dataloader):
 #     pass
